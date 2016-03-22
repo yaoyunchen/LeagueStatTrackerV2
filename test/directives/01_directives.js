520c53ce6c0f5338249e2a09acca45bad2e6c677
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-03-22 04:14:03 -0700

    added more testing

---
+++ test/directives/01_directives.js
@@ -0,0 +1,36 @@
+// MAINCONTROLLER TESTS
+
+// Set to test.
+process.env.NODE_ENV = 'test';
+
+// Dependencies.
+var app = require('../../app');
+require('../_test-helper');
+require('../../public/javascripts/angularApp.js');
+require('../../public/javascripts/factories.js');
+require('../../public/javascripts/directives.js');
+
+describe('directives', function() {
+  // var $compile, $rootScope;
+  // var ngRoute = {};
+  // var ngAnimate = {};
+  // var ngDialog = {};
+  // var angularCharts = {};
+  // // Load the app module which contains the directives.
+  // beforeEach(function() {
+  //   angular.mock.module('LeagueStatTrackerApp', [ngRoute, ngAnimate, ngDialog, angularCharts]);
+  // });
+
+  // beforeEach(inject(function($compile, $rootScope) {
+  //   $scope = $rootScope.new();
+  // }));
+
+  // describe('homepage', function() {
+  //   it('should automatically render to "/"', function() {
+
+  //   })
+  // });
+
+
+
+});       // directives
